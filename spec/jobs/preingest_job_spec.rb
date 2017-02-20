a2553e85a8fe6cbf55bcd647a866f7f5818f820f
Adam Ploshay <aploshay@iu.edu>
2017-02-20 08:10:47 -0500

    Added spec for preingest job

--- spec/jobs/preingest_job_spec.rb
+++ spec/jobs/preingest_job_spec.rb
@@ -1,36 +1,31 @@
-# to test app/jobs/preingest_job.rb
-# there also needs to be a corresponding rake task in lib/tasks; I've copied the pmp one
 require 'rails_helper'
+require './lib/hydradam/preingest/IU/tarball'
+require './lib/hydradam/preingest/IU/yaml'
 
 RSpec.describe PreingestJob do
-  let(:user) { FactoryGirl.build(:admin) }
+  let(:document_class) { nil }
+  let(:preingest_file) { '' }
+  let(:user) { User.first_or_create!(email: 'test@example.com', password: 'password') }
+  let(:yaml_file) { '' }
   shared_examples "successfully preingests" do
     it "writes the expected yaml output" do
       yaml_content = File.open(yaml_file) { |f| Psych.load(f) }
-      yaml_content[:sources][0][:file] = Rails.root.join(yaml_content[:sources][0][:file]).to_s
       expect(File).to receive(:write).with(yaml_file, yaml_content.to_yaml)
       described_class.perform_now(document_class, preingest_file, user)
     end
   end
 
-  skip "preingesting a METS file" do # FIXME: rewrite
-    let(:mets_file_single) { Rails.root.join("spec", "fixtures", "pudl_mets", "pudl0001-4612596.mets").to_s }
-    let(:mets_file_rtl) { Rails.root.join("spec", "fixtures", "pudl_mets", "pudl0032-ns73.mets").to_s }
-    let(:mets_file_multi) { Rails.root.join("spec", "fixtures", "pudl_mets", "pudl0001-4609321-s42.mets").to_s }
-    let(:yaml_file) { preingest_file.sub(/\.mets$/, '.yml') }
-    let(:document_class) { PreingestableMETS }
+  context "for IU YAML" do
+    let(:document_class) { HydraDAM::Preingest::IU::Yaml }
+    let(:preingest_file) { Rails.root.join("spec", "fixtures", "IU", "sip.yml").to_s }
+    let(:yaml_file) { preingest_file }
+    include_examples "successfully preingests"
+  end
 
-    context "with a single-volume mets file", vcr: { cassette_name: 'bibdata-bhr9405' } do
-      let(:preingest_file) { mets_file_single }
-      include_examples "successfully preingests"
-    end
-    context "with a right-to-left mets file", vcr: { cassette_name: 'bibdata-bhr9405' } do
-      let(:preingest_file) { mets_file_rtl }
-      include_examples "successfully preingests"
-    end
-    context "preingests a multi-volume yaml file", vcr: { cassette_name: 'bibdata-bhr9405' } do
-      let(:preingest_file) { mets_file_multi }
-      include_examples "successfully preingests"
-    end
+  context "for an IU tarball" do
+    let(:document_class) { HydraDAM::Preingest::IU::Tarball }
+    let(:preingest_file) { Rails.root.join("spec", "fixtures", "IU", "sip.tar").to_s }
+    let(:yaml_file) { preingest_file.sub(/\.tar$/, '.yml') }
+    # include_examples "successfully preingests"
   end
 end
