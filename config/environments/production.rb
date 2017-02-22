868caaa2cebac5f43ba4c7ddc8e27f2f15bb9c99
Adam Ploshay <aploshay@iu.edu>
2017-02-22 12:01:47 -0500

    [HDM-841] Change job queue handling to Inline

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # for Active Job Inline
+  config.active_job.queue_adapter = :inline
 end
