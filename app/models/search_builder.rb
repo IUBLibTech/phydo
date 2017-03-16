f1b9b2d3ab462549019d6d4cad8f746461de5675
Andrew Myers <afredmyers@gmail.com>
2017-03-16 00:56:48 -0600

    Adds ingest date search filter
    * Runs bundle update, specifically to bring in
      latest Preservation gem from github.
    * Removes duplicate inclusion of module
    * Updates FileSetPresenter; Uses newly available method,
      Preservation::PremisEventType.find_by_abbr().
    * Indexes ingest date on file sets
    * Associates file set with preservation events
    * Adds search filters for ingest date
    * Overrides curation_concerns catalog#index

--- app/models/search_builder.rb
+++ app/models/search_builder.rb
@@ -2,11 +2,9 @@
 class SearchBuilder < Blacklight::SearchBuilder
   include Blacklight::Solr::SearchBuilderBehavior
   include BlacklightRangeLimit::RangeLimitBuilder
-
   include BlacklightAdvancedSearch::AdvancedSearchBuilder
-
-  include BlacklightRangeLimit::RangeLimitBuilder
-  self.default_processor_chain += [:add_advanced_parse_q_to_solr, :add_advanced_search_to_solr]
+  # TODO: Where do these come from, and why are we adding them here?
+  self.default_processor_chain += [:add_advanced_parse_q_to_solr, :add_advanced_search_to_solr, :apply_ingest_date_time_filter]
   include Hydra::AccessControlsEnforcement
   include CurationConcerns::SearchFilters
 
@@ -27,4 +25,42 @@ class SearchBuilder < Blacklight::SearchBuilder
   def collection_clauses
     [ActiveFedora::SolrQueryBuilder.construct_query_for_rel(has_model: ::Collection.to_rdf_representation)]
   end
+
+
+  def apply_ingest_date_time_filter(solr_params)
+    if ingest_date_time_filter
+      solr_params[:fq] ||= []
+      solr_params[:fq] << ingest_date_time_filter
+    end
+    solr_params
+  end
+
+  private
+
+    def ingest_date_time_filter
+      @ingest_date_time_filter ||= begin
+        if ingest_date_time_before || ingest_date_time_after
+          range = "#{ingest_date_time_after || '*'} TO #{ingest_date_time_before || '*'}"
+          "ingest_date_time_dtsim:[#{range}]"
+        end
+      end
+    end
+
+    # Returns the 'before' date time formatted for a Solr query.
+    def ingest_date_time_before
+      @ingest_date_time_before ||= formatted_ingest_date_time(blacklight_params['ingest_date_time_before'])
+    end
+
+    # Returns the 'after' date time formatted for a Solr query.
+    def ingest_date_time_after
+      @ingest_date_time_after ||= formatted_ingest_date_time(blacklight_params['ingest_date_time_after'])
+    end
+
+    # Converts an unformatted date (as passed in via URL) to a date formatted
+    # for a Solr query.
+    def formatted_ingest_date_time(unformatted_date)
+      DateTime.parse(unformatted_date.to_s).utc.strftime("%Y-%m-%dT%H:%M:%SZ")
+    rescue ArgumentError => e
+      nil
+    end
 end
