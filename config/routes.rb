86b5a93c6f33025c58682c6a4dd66d668379334e
amolmkhedkar <akhedkar@iu.edu>
2017-04-19 10:11:09 -0400

    replace_CC_with_Hyrax in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -14,12 +14,12 @@ Rails.application.routes.draw do
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
-  mount CurationConcerns::Engine, at: '/'
+  mount Hyrax::Engine, at: '/'
   resources :welcome, only: 'index'
   root 'welcome#index'
-  curation_concerns_collections
-  curation_concerns_basic_routes
-  curation_concerns_embargo_management
+  hyrax_collections
+  hyrax_basic_routes
+  hyrax_embargo_management
   concern :exportable, Blacklight::Routes::Exportable.new
 
   resources :solr_documents, only: [:show], path: '/catalog', controller: 'catalog' do
@@ -35,10 +35,10 @@ Rails.application.routes.draw do
   end
 
   # Adds routes to large file interactions provided through Phydo::StorageControllerBehavior
-  get '/concern/file_sets/:id/file_status' => 'curation_concerns/file_sets#file_status', as: :file_status_curation_concerns_file_set
-  get '/concern/file_sets/:id/stage' => 'curation_concerns/file_sets#stage', as: :stage_curation_concerns_file_set
-  get '/concern/file_sets/:id/unstage' => 'curation_concerns/file_sets#unstage', as: :unstage_curation_concerns_file_set
-  get '/concern/file_sets/:id/fixity' => 'curation_concerns/file_sets#fixity', as: :fixity_curation_concerns_file_set
+  get '/concern/file_sets/:id/file_status' => 'hyrax/file_sets#file_status', as: :file_status_hyrax_file_set
+  get '/concern/file_sets/:id/stage' => 'hyrax/file_sets#stage', as: :stage_hyrax_file_set
+  get '/concern/file_sets/:id/unstage' => 'hyrax/file_sets#unstage', as: :unstage_hyrax_file_set
+  get '/concern/file_sets/:id/fixity' => 'hyrax/file_sets#fixity', as: :fixity_hyrax_file_set
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
