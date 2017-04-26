c57e1ea22c115bef148201bc8701f2a7e9b7f2af
amolmkhedkar <akhedkar@iu.edu>
2017-04-26 10:56:16 -0400

    replace_CC_with_Hyrax in work_form.rb

--- app/forms/curation_concerns/work_form.rb
+++ app/forms/curation_concerns/work_form.rb
@@ -1,7 +1,7 @@
 # Generated via
-#  `rails generate curation_concerns:work Work`
-module CurationConcerns
-  class WorkForm < CurationConcerns::Forms::WorkForm
+#  `rails generate hyrax:work Work`
+module Hyrax
+  class WorkForm < Hyrax::Forms::WorkForm
     self.model_class = ::Work
   end
 end
