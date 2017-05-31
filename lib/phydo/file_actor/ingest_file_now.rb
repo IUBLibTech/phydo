498061ab3d67a10959dc1385f3f0e3cf8e073033
Adam Ploshay <aploshay@iu.edu>
2017-05-31 15:13:17 -0400

    Fixes errors due to wrong number of arguments

--- lib/phydo/file_actor/ingest_file_now.rb
+++ lib/phydo/file_actor/ingest_file_now.rb
@@ -3,7 +3,7 @@ module Phydo
     module IngestFileNow
       # Monkeypatch to change the perform_later call to perform_now
       # Resolves bug with 2nd+ files not attaching to FileSet
-      def ingest_file(file)
+      def ingest_file(file, asynchronous)
         IngestFileJob.perform_now(
           file_set,
           working_file(file),
