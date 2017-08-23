fbc88fb2c7ef0363d5d75fe924f95aed633251e1
Andrew Myers <afredmyers@gmail.com>
2017-08-23 16:11:50 -0600

    Adds a test for WGBH batch ingest
    142 SIPs exported from WGBH FileMaker DAMS
    
    Initials for larger batch ingest test
    
    Renamed files in test WGBH ingest batch
    
    Changes source file names in ingest config

---
+++ spec/features/wgbh_ingest/example_3/ingest_example_3_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+require 'hyrax/ingest/batch_runner'
+
+RSpec.describe "WGBH ingest example 3 batch" do
+
+  let(:sip_paths) { Dir.glob('./spec/features/wgbh_ingest/example_3/example_3_batch/*') }
+  let(:num_sips) { sip_paths.count }
+
+  before do
+    @runner = Hyrax::Ingest::BatchRunner.new(config_file_path: File.expand_path('../ingest_example_3_config.yml', __FILE__), sip_paths: sip_paths)
+    @runner.run!
+  end
+
+  let(:file_sets) { FileSet.all }
+
+  it 'ingests all of the SIPs', :clean_fedora do
+    expect(file_sets.count).to eq num_sips
+  end
+end
