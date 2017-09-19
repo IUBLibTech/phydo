94c9868e43808f60862aa2c52ba1185ae90c45c5
Andrew Myers <afredmyers@gmail.com>
2017-09-19 07:58:56 -0600

    Adds spec for ingesting Preservation Events
    * Ingests Preservation Events related to FileSet objects.
    * Updates the bundle.

--- spec/features/wgbh_ingest/example_3/ingest_example_3_spec.rb
+++ spec/features/wgbh_ingest/example_3/ingest_example_3_spec.rb
@@ -2,18 +2,30 @@ require 'rails_helper'
 require 'hyrax/ingest/batch_runner'
 
 RSpec.describe "WGBH ingest example 3 batch" do
-
-  let(:sip_paths) { Dir.glob('./spec/features/wgbh_ingest/example_3/example_3_batch/*') }
-  let(:num_sips) { sip_paths.count }
-
+  # TODO: Currently the Fedora cleaning mechanism operates per each test. To speed things up
+  # have Fedora only be cleaned once per context.
   before do
-    @runner = Hyrax::Ingest::BatchRunner.new(config_file_path: File.expand_path('../ingest_example_3_config.yml', __FILE__), sip_paths: sip_paths)
+    @sip_paths = Dir.glob('./spec/features/wgbh_ingest/example_3/example_3_batch/*')
+    @runner = Hyrax::Ingest::BatchRunner.new(config_file_path: File.expand_path('../ingest_example_3_config.yml', __FILE__), sip_paths: @sip_paths)
     @runner.run!
   end
 
-  let(:file_sets) { FileSet.all }
-
   it 'ingests all of the SIPs', :clean_fedora, :large_ingest do
-    expect(file_sets.count).to eq num_sips
+    expect(FileSet.all.count).to eq @sip_paths.count
+  end
+
+  it 'ingests a single preservation event with PREMIS event type of "Ingest" and PREMIS agent of mailto:admin@example.org', :clean_fedora, :large_ingest do
+    FileSet.all.each do |file_set|
+      # Expect exactly 1 preservation event.
+      expect(file_set.preservation_events.count).to eq 1
+
+      # Expect the PREMIS event type to be that which was specified in the ingest config YAML.
+      premis_event_type_uri = file_set.preservation_events.first.premis_event_type.first.to_uri.to_s
+      expect(premis_event_type_uri).to eq 'http://id.loc.gov/vocabulary/preservation/eventType/ing'
+
+      # Expect the PREMIS agent to be that which was specified in the ingest config YAML.
+      premis_agent = file_set.preservation_events.first.premis_agent.first.to_uri.to_s
+      expect(premis_agent).to eq 'mailto:admin@example.org'
+    end
   end
 end
