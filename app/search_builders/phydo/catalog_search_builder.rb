c49017b060b5dee23187e955a9c9550e16b5f854
Andrew Myers <afredmyers@gmail.com>
2017-09-26 15:38:19 -0600

    Ingest sample data with Fixity preservation events
    Also...
    * Updates hyrax-preservation and hyrax-ingest gems to take advantage of new
      DateTime fetcher, and ToDate transformer.
    * Update Solr version in SolrWrapper config. Note this will conflict with
      the same change made upstream after a rebase.

--- app/search_builders/phydo/catalog_search_builder.rb
+++ app/search_builders/phydo/catalog_search_builder.rb
@@ -1,7 +1,50 @@
 module Phydo
   class CatalogSearchBuilder < Hyrax::CatalogSearchBuilder
+
+    self.default_processor_chain += [:apply_last_fixity_date_time_filter]
+
     def models
       super + [::FileSet]
     end
+
+    def apply_last_fixity_date_time_filter(solr_params)
+      if last_fixity_date_time_filter
+        solr_params[:fq] ||= []
+        solr_params[:fq] << last_fixity_date_time_filter
+      end
+      solr_params
+    end
+
+
+    private
+
+      # Returns a date/time range for a Solr query for the 'after' and 'before'
+      # URL params.
+      def last_fixity_date_time_filter
+        @last_fixity_date_time_filter ||= begin
+          if last_fixity_date_time_before || last_fixity_date_time_after
+            range = "#{last_fixity_date_time_after || '*'} TO #{last_fixity_date_time_before || '*'}"
+            "last_fixity_date_time_dtsim:[#{range}]"
+          end
+        end
+      end
+
+      # Returns 'after' date time of last fixity check, formatted for a Solr query.
+      def last_fixity_date_time_after
+        @last_fixity_date_time_after ||= formatted_last_fixity_date_time(blacklight_params['last_fixity_date_time_after'])
+      end
+
+      # Returns 'after' date time of last fixity check, formatted for a Solr query.
+      def last_fixity_date_time_before
+        @last_fixity_date_time_before ||= formatted_last_fixity_date_time(blacklight_params['last_fixity_date_time_before'])
+      end
+
+      # Converts an unformatted date (as passed in via URL) to a date formatted
+      # for a Solr query.
+      def formatted_last_fixity_date_time(unformatted_date)
+        DateTime.parse(unformatted_date.to_s).utc.strftime("%Y-%m-%dT%H:%M:%SZ")
+      rescue ArgumentError => e
+        nil
+      end
   end
 end
